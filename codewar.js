bfefc19ca65c502176db11ee819485b38a564736
ShabsCreativeContent <ajideshedrack@gmail.com>
2024-04-01 16:31:37 +0100

    CodeWar

--- codewar.js
+++ codewar.js
@@ -101,3 +101,33 @@ function disemvowel(str) {
 }
 
 console.log(disemvowel("shedRAck love food"));
+
+
+
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+const allSheep = [true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+function countSheeps(sheep) {
+  let presentSheep = 0
+
+  sheep.forEach(present => {
+    if(present){
+      presentSheep++
+    }
+  })
+
+  console.log(presentSheep);
+}
+
+countSheeps(allSheep)
