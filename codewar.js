128a24cab6a0e3fba54b2cd94fd31adeaea6028a
ShabsCreativeContent <ajideshedrack@gmail.com>
2024-04-21 13:01:49 +0100

    codewar

--- codewar.js
+++ codewar.js
@@ -273,3 +273,22 @@ console.log(sum([]));
 function sum(numbers) {
   return numbers.reduce((a, b) => a + b, 0);
 }
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  //...
+  if (bool) {
+    return "Yes"
+  } else {
+    return "No"
+  }
+}
+
+console.log(boolToWord(false));
+
+// OR
+
+function boolToWord1( bool ){
+  return bool ? 'Yes':'No';
+}
