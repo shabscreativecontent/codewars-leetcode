42ff4a094cedd3daf97ceb04a93083405b16c71f
ShabsCreativeContent <ajideshedrack@gmail.com>
2024-04-25 09:56:44 +0100

    React code

--- React.js
+++ React.js
@@ -45,3 +45,104 @@ function ClassNote() {
       </main>
    )
 }
+
+
+function App() {
+   const [squares, setSquares] = React.useState(boxes)
+   
+   function toggle(id) {
+       setSquares(prevSquares => {
+           const newSquares = []
+           for(let i = 0; i < prevSquares.length; i++) {
+               const currentSquare = prevSquares[i]
+               if(currentSquare.id === id) {
+                   const updatedSquare = {
+                       ...currentSquare,
+                       on: !currentSquare.on
+                   }
+                   newSquares.push(updatedSquare)
+               } else {
+                   newSquares.push(currentSquare)
+               }
+           }
+           return newSquares
+       })
+   }
+   
+   const squareElements = squares.map(square => (
+       <Box 
+           key={square.id} 
+           id={square.id}
+           on={square.on} 
+           toggle={toggle}
+       />
+   ))
+   
+   return (
+       <main>
+           {squareElements}
+       </main>
+   )
+}
+
+
+
+function Box(props) {
+   const styles = {
+      backgroundColor: props.on ? "#222222" : "transparent"
+   }
+   
+   return (
+      <div 
+      style={styles} 
+      className="box"
+      onClick={()=>props.toggle(props.id)}
+      >
+       </div>
+   )
+}
+
+// OR THIS WAY UNDER
+
+
+export default function App2() {
+   const [squares, setSquares] = React.useState(boxes)
+   
+   function toggle(id) {
+       setSquares(prevSquares => {
+           return prevSquares.map(square => {
+            return square.id === id ? {...square, on: !square.on} : square
+           })
+       })
+   }
+   
+   const squareElements = squares.map(square => (
+       <Box 
+           key={square.id}
+           on={square.on} 
+           toggle={() => toggle(square.id)}
+       />
+   ))
+   
+   return (
+       <main>
+           {squareElements}
+       </main>
+   )
+}
+
+
+function Box(props) {
+   const styles = {
+      backgroundColor: props.on ? "#222222" : "transparent"
+   }
+   
+   return (
+      <div 
+      style={styles} 
+      className="box"
+      onClick={props.toggle}
+      >
+       </div>
+   )
+}
